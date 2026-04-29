7791e2a9ddb30f88100c2fbfdfcf8e004acee273
z4nshin <z4nshin@gmail.com>
2026-04-29 16:20:10 +0200

    Formated all the files with LSP

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub const LexerToken = union (enum) {
+pub const LexerToken = union(enum) {
     float: f32,
     // string: []const u8,
     int: i32,
@@ -13,93 +13,94 @@ pub const LexerToken = union (enum) {
 };
 
 fn panic(msg: []const u8, filename: []const u8, linenr: i32, function_name: []const u8) void {
-    std.debug.print("{s}\n\t{s}:{d} -- {s}(...)\n", .{msg, filename, linenr, function_name});
+    std.debug.print("{s}\n\t{s}:{d} -- {s}(...)\n", .{ msg, filename, linenr, function_name });
     std.debug.assert(false);
 }
 
-pub fn print(tokens:[]LexerToken) void {
+pub fn print(tokens: []LexerToken) void {
     for (tokens) |t| {
-            switch (t) {
-                .add => {std.debug.print("ADD\n", .{});},
-                .sub => {std.debug.print("SUB\n", .{});},
-                .mul => {std.debug.print("MUL\n", .{});},
-                .div => {std.debug.print("DIV\n", .{});},
-                .int => |v|{
-                    std.debug.print("{d}\n", .{v});
-                },
-                .float => |v| {
-                    std.debug.print("{d}\n", .{v});
+        switch (t) {
+            .add => {
+                std.debug.print("ADD\n", .{});
             },
-                .unknown => {break;}
-            }
+            .sub => {
+                std.debug.print("SUB\n", .{});
+            },
+            .mul => {
+                std.debug.print("MUL\n", .{});
+            },
+            .div => {
+                std.debug.print("DIV\n", .{});
+            },
+            .int => |v| {
+                std.debug.print("{d}\n", .{v});
+            },
+            .float => |v| {
+                std.debug.print("{d}\n", .{v});
+            },
+            .unknown => {
+                break;
+            },
         }
+    }
 }
 pub fn run(alloc: std.mem.Allocator, data: []const u8) ![]LexerToken {
-    var index:usize = 0;
-    var token_index:usize = 0;
+    var index: usize = 0;
+    var token_index: usize = 0;
     const tokens = try alloc.create([1024]LexerToken);
     for (tokens) |*t| {
-        t.* = LexerToken{.unknown = true};
+        t.* = LexerToken{ .unknown = true };
     }
 
-    while (index < data.len) : (index+=1) {
+    while (index < data.len) : (index += 1) {
         if (std.ascii.isWhitespace(data[index])) continue;
 
         switch (data[index]) {
-            '0'...'9' =>  {
+            '0'...'9' => {
                 var i = index;
-                while (i < data.len and std.ascii.isDigit(data[i])) : (i+=1) {}
+                while (i < data.len and std.ascii.isDigit(data[i])) : (i += 1) {}
                 const n = try std.fmt.parseInt(i32, data[index..i], 10);
                 if (i < data.len and data[i] == '.') {
-                    i+=1;
-                    while (i < data.len and std.ascii.isDigit(data[i])) : (i+=1) {}
+                    i += 1;
+                    while (i < data.len and std.ascii.isDigit(data[i])) : (i += 1) {}
                     const f = try std.fmt.parseFloat(f32, data[index..i]);
-                    tokens[token_index] = LexerToken{.float = f};
+                    tokens[token_index] = LexerToken{ .float = f };
                 } else {
-                    tokens[token_index] = LexerToken{.int = n};
+                    tokens[token_index] = LexerToken{ .int = n };
                 }
-                index = i-1;
+                index = i - 1;
                 token_index += 1;
             },
-            '+', '*', '-', '/' => |o|{
+            '+', '*', '-', '/' => |o| {
                 switch (o) {
                     '+' => {
-                tokens[token_index] = LexerToken{.add={}};
-                token_index += 1;
-                
-                
-                },
+                        tokens[token_index] = LexerToken{ .add = {} };
+                        token_index += 1;
+                    },
                     '-' => {
-                tokens[token_index] = LexerToken{.sub={}};
-                token_index += 1;
-                
-                
-                },
+                        tokens[token_index] = LexerToken{ .sub = {} };
+                        token_index += 1;
+                    },
                     '*' => {
-                tokens[token_index] = LexerToken{.mul={}};
-                token_index += 1;
-                
-                
-                },
+                        tokens[token_index] = LexerToken{ .mul = {} };
+                        token_index += 1;
+                    },
                     '/' => {
-                tokens[token_index] = LexerToken{.div={}};
-                token_index += 1;
-                
-                
+                        tokens[token_index] = LexerToken{ .div = {} };
+                        token_index += 1;
                     },
                     else => {
-                const s = @src();
-                panic("TODO: operator", s.file, s.line, s.fn_name );
-                },
-            }
-        },
-        else => |t|{
+                        const s = @src();
+                        panic("TODO: operator", s.file, s.line, s.fn_name);
+                    },
+                }
+            },
+            else => |t| {
                 const s = @src();
                 const msg = try std.fmt.allocPrint(alloc, "UNKNOWN TOKEN '{c}'\n", .{t});
-                panic(msg, s.file, s.line, s.fn_name );
+                panic(msg, s.file, s.line, s.fn_name);
+            },
         }
-
-    }
     }
     return tokens;
 }
--- src/main.zig
+++ src/main.zig
@@ -9,30 +9,29 @@ pub fn main(init: std.process.Init) !void {
     var read_buffer: [1024]u8 = undefined;
 
     var stdout_writer: std.Io.File.Writer = .init(.stdout(), init.io, &write_buffer);
-    
+
     var wiface = &stdout_writer.interface;
 
-    var stdin_reader: std.Io.File.Reader = .init( .stdin(), init.io, &read_buffer);
+    var stdin_reader: std.Io.File.Reader = .init(.stdin(), init.io, &read_buffer);
 
     var riface = &stdin_reader.interface;
 
     repl: while (true) {
-        var buffer:[512]u8 = undefined;
+        var buffer: [512]u8 = undefined;
         var bwriter = std.Io.Writer.fixed(&buffer);
         try wiface.print("> ", .{});
         try wiface.flush();
-        const l  = try riface.streamDelimiter(&bwriter, '\n');
+        const l = try riface.streamDelimiter(&bwriter, '\n');
         // _ = try riface.discardRemaining();
         _ = try riface.discardShort(1);
 
         // try wiface.print("{s}\n", .{buffer[0..l]});
         // try wiface.flush();
-        if (std.mem.eql(u8, "/quit", buffer[0..l])){
+        if (std.mem.eql(u8, "/quit", buffer[0..l])) {
             break :repl;
         }
 
-        
-        const tokens  = try lex.run(init.arena.allocator(), buffer[0..l]);
+        const tokens = try lex.run(init.arena.allocator(), buffer[0..l]);
         lex.print(tokens);
     }
     //
