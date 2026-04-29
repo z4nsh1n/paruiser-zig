2bdd9558a730821e1db04028f3c6c2e8ad7c5f1a
z4nshin <z4nshin@gmail.com>
2026-04-30 00:31:57 +0200

    Added start of terms (addition) to the parser. It's recursive now.

--- src/parser.zig
+++ src/parser.zig
@@ -23,6 +23,7 @@ const ast = union(enum) {
     String: []const u8,
     Expr: struct { op: Operator, left: *const ast, right: *const ast },
     Stmts: []ast,
+    END: void,
 };
 
 pub fn print(a: *const ast) void {
@@ -38,6 +39,9 @@ pub fn print(a: *const ast) void {
             print(v.left);
             print(v.right);
             std.debug.print(")\n", .{});
+        },
+        .END => {
+            std.debug.print("END", .{});
         },
         else => {},
     }
@@ -79,15 +83,39 @@ pub fn parse_expr(alloc: std.mem.Allocator, tokens: []lex.LexerToken) !*ast {
     }
     return a;
 }
+
+pub fn parse_term(alloc: std.mem.Allocator, tokens: []lex.LexerToken) !*ast {
+    const left = try parse_expr(alloc, tokens);
+    var a = left;
+    while (switch (tokens[token_idx]) {
+        .eof => false,
+        else => true,
+    }) {
+        switch (tokens[token_idx]) {
+            .add => {
+                token_idx += 1;
+                a = try alloc.create(ast);
+                a.* = .{
+                    .Expr = .{.op = .ADD, .left = left, .right = try parse_term(alloc, tokens)}
+                };
+            },
+            .sub => {},
+            else => {break;}
+        }
+    }
+    return a;
+}
 pub fn run(alloc: std.mem.Allocator, lex_tokens: []lex.LexerToken) !*ast {
-    var a: *ast = undefined;
+    var a: *ast = try alloc.create(ast);
+    a.* = ast{.END = {}};
     while (switch (lex_tokens[token_idx]) {
         .eof => false,
         else => true,
     }) {
         std.debug.print("idx: {d}\n", .{token_idx});
-        a = try parse_expr(alloc, lex_tokens);
+        a = try parse_term(alloc, lex_tokens);
     }
+
     // reset token_idx for the next time!
     token_idx = 0;
     return a;
